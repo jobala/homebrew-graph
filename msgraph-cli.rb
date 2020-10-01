a7c0b3bf5d1b6c2842dcd235cab0665eed086443
jobala <japhethobalak@gmail.com>
2020-10-01 13:01:51 +0300

    Update sha256

--- msgraph-cli.rb
+++ msgraph-cli.rb
@@ -1,8 +1,8 @@
 class MsgraphCli < Formula
   desc "Python based command line tools for interacting with Microsoft Graph"
   homepage ""
-  url "https://github.com/microsoftgraph/msgraph-cli/archive/0.0.1.tar.gz"
-  sha256 "3571d8559ad7563e849c537a63595adfbb06602afc37e660c3cec184f14e8b51"
+   url "https://github.com/microsoftgraph/msgraph-cli/archive/0.0.2.tar.gz"
+   sha256 "93b08b1462b8e5f9331620cb6537f0bbf39f6a477734d9cd464b02667713142d"
   license "NOASSERTION"
 
   def install
